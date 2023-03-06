be45abbbfa12312bd25f95d3ecdf00df38e23d7e
Elias Jimenez Diz <eliasj.diz@gmail.com>
2023-03-06 12:45:00 -0500

    fixed

--- src/components/FormManga/FormManga.jsx
+++ src/components/FormManga/FormManga.jsx
@@ -1,18 +1,43 @@
 import React from 'react'
 import './formmanga.css'
 import SelectManga from '../SelectManga/Selectmanga'
+import { useRef } from 'react'
+import axios from 'axios'
 
 
 
-export default function FormManga(props) {
+export default function FormManga() {
+
+    let tittle = useRef()
+    let category = useRef()
+    let description = useRef()
+    
+    async function handleSubmit(e){
+        e.preventDefault()
+        let data = {
+            [tittle.current.name]: tittle.current.value,
+            [category.current.name]: category.current.value,
+            [description.current.name]: description.current.value
+        }
+        let url = 'http://localhost:8080/mangas'
+        try {
+            await axios.post(url,data) 
+            alert('Manga created susccesfully')
+        } catch (error) {
+            console.log(error.response.data.response);
+            alert(error.response.data.message)
+        }
+        e.target.reset()
+    }
+
     return (
-        <div className='manga'>
+        <div className='manga' onSubmit={handleSubmit}>
             <h1>New Manga</h1>
             <form className='formManga'>
-                <input type='text' placeholder='Insert title'/>
-                <SelectManga />
-                <input type='text' placeholder='Insert description'/>
-                <input className='button' type='submit' value='Send' />
+                <input type='text' placeholder='Insert tittle' ref={tittle} name='tittle'/>
+                <SelectManga parentref={category} name='category' />
+                <input type='text' placeholder='Insert description' ref={description} name='description'/>
+                <input className='submit' type='submit' value='Send' />
             </form>
             </div>
     )
