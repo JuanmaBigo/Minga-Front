8e3a0a732d42feab50a2c005697cadc7d2a45d64
Johan Zuluaga <johan16zulu@gmail.com>
2023-03-11 19:30:09 -0500

    implementacion cambio de color por type category

--- src/components/CardsManga/CardsManga.jsx
+++ src/components/CardsManga/CardsManga.jsx
@@ -13,10 +13,10 @@ export default function CardsManga(props) {
 
   return (
     <div className='card'>
-      <span className='span-card'></span>
+      <span className={`span-card ${props.category_.name.includes('shonen') ? 'red-span' : props.category_.name.includes('comic')?'orange-span':props.category_.name.includes('shojo')?'green-span':props.category_.name.includes('seinen')?'purple-span':''}` }></span>
       <div className='inf-card'>
         <h2 className='title-card'>{props.title_}</h2>
-        <h3 className='type-card'>{props.category_.name}</h3>
+        <h3 className={`type-card ${props.category_.name.includes('shonen') ? 'red-type' : props.category_.name.includes('comic')?'orange-type':props.category_.name.includes('shojo')?'green-type':props.category_.name.includes('seinen')?'purple-type':''}`}>{props.category_.name}</h3>
         <button onClick={handleNavegate} className='btn-card'>Read</button>
       </div>
       <img className='img-card' src={props.photo} alt="naruto" />
