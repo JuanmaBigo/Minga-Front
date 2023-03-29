9365df7a523e8bf057c678bf1ca9368116287b89
Juan Mateo Bigoritto <66440535+JuanmaBigo@users.noreply.github.com>
2023-03-29 13:28:17 -0300

    Update MainLayout.jsx
    deleted ScrollToTop component

--- src/layouts/MainLayout/MainLayout.jsx
+++ src/layouts/MainLayout/MainLayout.jsx
@@ -3,12 +3,10 @@ import Header from '../../components/Header/Header'
 import Footer from '../../components/Footer/Footer'
 import Alert from '../../components/Alert/Alert'
 import { Outlet } from 'react-router-dom'
-import ScrollToTop from '../../components/ScrollToTop/ScrollToTop'
 
 export default function LayoutMain(props) {
   return (
     <div className="main-container">
-      <ScrollToTop />
       <Header />
       <Outlet />
       <Footer />
